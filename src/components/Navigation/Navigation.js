169576a52bf410932336000fd91eb16f321578f1
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-04-04 22:27:03 +0100

    update nav structure

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <ul>
-      <NavLink to="/">
-        <li>Home</li>
-      </NavLink>
-      <NavLink to="/about">
-        <li>About</li>
-      </NavLink>
-      <NavLink to="/contact-us">
-        <li>Contact us</li>
-      </NavLink>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact-us">Contact us</NavLink>
+      </li>
       <a href="https://www.google.com">Google</a>
     </ul>
   );
