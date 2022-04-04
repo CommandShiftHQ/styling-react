6fac6022d05e00f58d186e3174faf1d5bb9522dc
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-04-04 20:07:03 +0100

    css stylesheets

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,17 +3,18 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-      <ul>
-        <NavLink to="/">
-          <li>Home</li>
-        </NavLink>
-        <NavLink to="/about">
-          <li>About</li>
-        </NavLink>
-        <NavLink to="/contact-us">
-          <li>Contact us</li>
-        </NavLink>
-      </ul>
+    <ul>
+      <NavLink to="/">
+        <li>Home</li>
+      </NavLink>
+      <NavLink to="/about">
+        <li>About</li>
+      </NavLink>
+      <NavLink to="/contact-us">
+        <li>Contact us</li>
+      </NavLink>
+      <a href="https://www.google.com">Google</a>
+    </ul>
   );
 };
 
